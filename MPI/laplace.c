7a67a8543b3df7322c167dd1c76e0b0cd73803eb
Jules-NC <jules.nc@protonmail.com>
2020-10-15 01:30:23 +0200

    refactoring

--- MPI/laplace.c
+++ MPI/laplace.c
@@ -7,8 +7,20 @@
 
 #include "mpi.h"
 
-void laplace(int N, int lignes, double * tab){
+typedef struct {
+	int NPROCS;
+	int rank;
+	MPI_Status status;
+	int N; // matrix length
+	int lines;
+	int internal_lines;
+	double * matrix;
+	double total_error;
+} NodeInfo;
+
+void laplace(double * reducedError, int rank, int N, int lignes, double * tab){
 	double fnew[N*lignes];
+	double error = 0;
 	for(int i=N; i<N*(lignes-1); i++){ 
 		if((i+1)%N*i%N == 0) { // If end or beginning of the line (i%N = begining and (i+1)%N = end)
 			fnew[i] = tab[i];
@@ -21,36 +33,45 @@ void laplace(int N, int lignes, double * tab){
 
 		double result = 0.25*(left+right+up+down);
 		fnew[i] = result;
+
+		error += (tab[i]-fnew[i])*(tab[i]-fnew[i]) ;
 	}
+
 	//memcpy(tab+N, fnew, N*(lignes-2)*sizeof(double));
 	for(int i=N; i<N*(lignes-1); i++){
 		tab[i] = fnew[i];	
 	}
+
+	MPI_Allreduce(&error, reducedError, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+
 }
 
 
-void share(int NPROC, int rank, int N, int lines, double* tab, MPI_Status status){
+void share(NodeInfo node){
 	/**
 	The order here is very important to avoid deadlocks !!!
 	Receive/send comes in pairs.
 	**/
-	if(NPROC == 1) return;
+	int N = node.N;
+	int rank = node.rank;
+	double * tab = node.matrix;
+	int lines = node.lines;
+
+	if(node.NPROCS == 1) return;
 	if(rank == 0){
-		MPI_Send(tab+N*(lines-2), N, MPI_DOUBLE, rank+1, 99, MPI_COMM_WORLD);
-		MPI_Recv(tab+N*(lines-1), N, MPI_DOUBLE, rank+1, 99, MPI_COMM_WORLD, &status);
+		MPI_Send(node.matrix+N*(lines-2), N, MPI_DOUBLE, rank+1, 99, MPI_COMM_WORLD);
+		MPI_Recv(tab+N*(lines-1), N, MPI_DOUBLE, rank+1, 99, MPI_COMM_WORLD, &node.status);
 	}
-	else if(rank == NPROC-1){
-		MPI_Recv(tab, N, MPI_DOUBLE, rank-1, 99, MPI_COMM_WORLD, &status);
+	else if(rank == node.NPROCS-1){
+		MPI_Recv(tab, N, MPI_DOUBLE, rank-1, 99, MPI_COMM_WORLD, &node.status);
 		MPI_Send(tab+N, N, MPI_DOUBLE, rank-1, 99, MPI_COMM_WORLD);
 	}
 	else{
 		// Receive from left, send to right | send to right, receive from left
-		MPI_Recv(tab, N, MPI_DOUBLE, rank-1, 99, MPI_COMM_WORLD, &status);
+		MPI_Recv(tab, N, MPI_DOUBLE, rank-1, 99, MPI_COMM_WORLD, &node.status);
 		MPI_Send(tab+N, N, MPI_DOUBLE, rank-1, 99, MPI_COMM_WORLD);
 		MPI_Send(tab+N*(lines-2), N, MPI_DOUBLE, rank+1, 99, MPI_COMM_WORLD);
-		MPI_Recv(tab+N*(lines-1), N, MPI_DOUBLE, rank+1, 99, MPI_COMM_WORLD, &status);
-
-		printf("proc %d receiveid all the thingy\n", rank);
+		MPI_Recv(tab+N*(lines-1), N, MPI_DOUBLE, rank+1, 99, MPI_COMM_WORLD, &node.status);
 	}
 
 	// wait for all the messages of all the processes to be received
@@ -92,61 +113,63 @@ void setLineToConst(double* tab, int size, double value){
 int main(int argc, char *argv[])
 {
 	char filename[250]="mat5000";
-	int N = 512;
-	int NPROC;
-	int RANK;
 	int nb_lignes;
-	double * localMatrix ;
-	int localMatrixLinesNumber;	
+	NodeInfo node = {.N = 512, .total_error = 0};
+	int N = node.N;
 
-	MPI_Status status;
 	MPI_Init(&argc, &argv);
-	MPI_Comm_rank(MPI_COMM_WORLD, &RANK);
-	MPI_Comm_size(MPI_COMM_WORLD, &NPROC);
+	MPI_Comm_rank(MPI_COMM_WORLD, &node.rank);
+	MPI_Comm_size(MPI_COMM_WORLD, &node.NPROCS);
+
 	
-	nb_lignes = N/NPROC;
+	nb_lignes = node.N/node.NPROCS;
+	node.internal_lines = nb_lignes;
 	if(nb_lignes<2) {
 		printf("Hey mon ami ! La taille des blocs aux extremites est de %d, donc %d avec le recouvrement, ce qui ne permet aps d'acceder a la valeur 'en dessous', il faut minimum 3 lignes en tout. BISOUS\n", nb_lignes, nb_lignes+1);
 		return -1;
 	}
 
 	// localmatrix init by NPROC ID (0 and NPROC-1 are 1 line smaller)
-	if(RANK==0 || RANK==NPROC-1){
-		localMatrix = malloc(sizeof(double)*N*(nb_lignes+1));
-		localMatrixLinesNumber = nb_lignes+1;
-		memset(localMatrix, 0, sizeof(double)*N*(nb_lignes+1));
+	if(node.rank==0 || node.rank==node.NPROCS-1){
+		node.matrix = malloc(sizeof(double)*N*(node.internal_lines+1));
+		node.lines = node.internal_lines+1;
+		memset(node.matrix, 0, sizeof(double)*N*(node.internal_lines+1));
 	} else {
-		localMatrix = malloc(sizeof(double)*N*(nb_lignes+2));
-		localMatrixLinesNumber = nb_lignes+2;
-		memset(localMatrix, 0, sizeof(double)*N*(nb_lignes+2));
+		node.matrix = malloc(sizeof(double)*N*(node.internal_lines+2));
+		node.lines = node.internal_lines+2;
+		memset(node.matrix, 0, sizeof(double)*N*(node.internal_lines+2));
 	}
 
-	int displacement = N;
-	if(RANK==0) displacement = 0;
+	int displacement = node.N;
+	if(node.rank==0) displacement = 0;
 
 	// load matrix to localMatrix
-	matrix_pload(filename, (localMatrix+displacement), N, NPROC, nb_lignes, RANK, status);
+	matrix_pload(filename, (node.matrix+displacement), node.N, node.NPROCS, node.internal_lines, node.rank, node.status);
 
 	// set first and last line to -1
-	if(RANK==0) setLineToConst(localMatrix, N, -1);
-	if(RANK==NPROC-1) setLineToConst(localMatrix+N*(nb_lignes), N, -1);
+	if(node.rank==0) setLineToConst(node.matrix, node.N, -1);
+	if(node.rank==node.NPROCS-1) setLineToConst(node.matrix+N*(node.lines-1), node.N, -1);
 
-	share(NPROC, RANK, N, localMatrixLinesNumber, localMatrix, status);
+	share(node);
 
-	if(RANK==1) localMatrix[16] = 420;
+	if(node.rank==1) node.matrix[16] = 420;
 
-	laplace(N, localMatrixLinesNumber, localMatrix);
+	laplace(&node.total_error, node.rank, N, node.lines, node.matrix);
 
-	share(NPROC, RANK, N, localMatrixLinesNumber, localMatrix, status);
+	if(node.rank==0){
+		node.total_error = sqrt(node.total_error);
+		printf("LOLI 28 DIT: %f\n", node.total_error);
+	}
+	share(node);
 
 
-	if (RANK == 0)
+	if (node.rank == 0)
 	{
-		printf("Size local matrix: %d\n", localMatrixLinesNumber);
-		for(int i=0; i<N*localMatrixLinesNumber; i++){
-			printf("%f ", localMatrix[i]);
+		printf("Size local matrix: %d\n", node.lines);
+		for(int i=0; i<N*node.lines; i++){
+			//printf("%f ", localMatrix[i]);
 		}
-		printf("\n");
+		//printf("\n");
 	}
 	MPI_Finalize();
 	return 0;
